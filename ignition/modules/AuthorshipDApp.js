9be93db70ebebb9241452fed6e674c02d21fdd0f
dreamsoftware92@gmail.com <dreamsoftware92@gmail.com>
2025-05-10 11:47:00 +0200

    Initial project configuration

---
+++ ignition/modules/AuthorshipDApp.js
@@ -0,0 +1,6 @@
+const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
+const secret = require('../../.secret.json');
+
+module.exports = buildModule("AuthorshipDApp", (m) => {
+    
+});
