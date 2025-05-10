f75bccba99f0608665c3ee6efaec6c10249c9fce
dreamsoftware92@gmail.com <dreamsoftware92@gmail.com>
2025-05-10 14:24:54 +0200

    Implement ignition module

--- ignition/modules/AuthorshipDApp.js
+++ ignition/modules/AuthorshipDApp.js
@@ -2,5 +2,7 @@ const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 const secret = require('../../.secret.json');
 
 module.exports = buildModule("AuthorshipDApp", (m) => {
-    
+    const rewardToken = m.contract("RewardToken", [secret.ownerKey]);
+    const authorshipDApp = m.contract("AuthorshipDApp", [secret.ownerKey, rewardToken]);
+    return { rewardToken, authorshipDApp };
 });
